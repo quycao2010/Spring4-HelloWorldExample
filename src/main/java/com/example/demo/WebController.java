19118c01f83cf0004f9a3463c22ec1ec83c57aaa
QuyNguyen <quycao2010@gmail.com>
2020-02-16 22:07:50 +0700

    Spring 4 Hello world example

---
+++ src/main/java/com/example/demo/WebController.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class WebController {
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+
+    @GetMapping("/about")
+    public String about(){
+        return "about";
+    }
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name") String name, Model model){
+        model.addAttribute("value",name);
+        return "hello";
+    }
+}
